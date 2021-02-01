74e6768f9a37dc1d3a7dcff8e147cfe066111cbb
wang <332084283@qq.com>
2021-02-01 17:58:48 +0800

    2.1提交

--- src/views/shop/Product.vue
+++ src/views/shop/Product.vue
@@ -40,7 +40,7 @@
           ></span>
         </template>
       </el-table-column>
-      <el-table-column label="操作">
+      <el-table-column label="操作"> 
         <template slot-scope="scope">
           <!-- <el-button type="text" @click="$router.push(`/product/detail/${scope.row.id}`)>查看</el-button> -->
           <el-button type="text">编辑</el-button>
